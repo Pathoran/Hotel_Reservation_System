e767bcc11e879a867af2fec4e6fa74af5cf2f0e4
William Beckler <wsbeckler@gmail.com>
2016-01-07 02:49:13 -0500

    Removed MASTERKEY and got CredentialCipher to correctly compare passwords.  Now to add the functionality of saving the passwords and salts to a separate file.

--- src/CredentialCipher.java
+++ src/CredentialCipher.java
@@ -3,65 +3,87 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.Arrays;
 
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.PBEParameterSpec;
-
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 /**
  * Created by William Beckler on 1/5/2016.
  */
 public class CredentialCipher {
 
-    private static final byte[] SALTSEQUENCE = {(byte) 0xde, (byte) 0x33, (byte) 0x10, (byte) 0x12, (byte) 0xde, (byte) 0x33, (byte) 0x10, (byte) 0x12};
+    private static final Random RANDOM = new SecureRandom();
+    private static final int ITERATIONCOUNT = 1000;
+    private static final int KEYLENGTH = 256;
 
     public static void main(String[] args) throws Exception {
-        String pwd = "secret";
+        Scanner scan = new Scanner(System.in);
+        String pwd = "secret0";
+        byte[] salt = generateSalt();
         System.out.println("Original password: " + pwd);
-        String encryptedPassword = encrypt(pwd);
-        System.out.println("Encrypted password: " + encryptedPassword);
+        char[] pwdChar = pwd.toCharArray();
+        byte[] orig = generateHash(pwdChar, salt);
+        String encrypted = Arrays.toString(orig);
+        System.out.println("Encrypted password: " + encrypted);
+        System.out.println("Enter password: ");
+        String blah = scan.nextLine();
+        char[] sChar = blah.toCharArray();
+        byte[] newh = generateHash(blah.toCharArray(), salt);
+        sChar = blah.toCharArray();
+        String check = Arrays.toString(newh);
+        System.out.println("Attempted password: " + check);
+        if (hashCheck(blah.toCharArray(), salt, orig)) {
+            System.out.println("It's a match!");
+        }
+        else {
+            System.out.println("It's not a match.");
+        }
     }
 
-    private static String getPassword() throws FileNotFoundException {
-
-        File file = new File("MASTERKEY.txt");
-        Scanner scan = new Scanner(file);
-        return scan.nextLine();
+    public static byte[] generateSalt() {
+        byte[] salt = new byte[32];
+        RANDOM.nextBytes(salt);
+        return salt;
     }
 
-    private static String base64Encode(byte[] bytes) {
-        //this class is internal, and you probably should use another impl
-        return new BASE64Encoder().encode(bytes);
+    public static byte[] generateHash(char[] password, byte[] salt) throws GeneralSecurityException {
+        PBEKeySpec key = new PBEKeySpec(password, salt, ITERATIONCOUNT, KEYLENGTH);
+        Arrays.fill(password, Character.MIN_VALUE);
+        SecretKeyFactory secret = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        return secret.generateSecret(key).getEncoded();
     }
 
-    private static byte[] base64Decode(String property) throws IOException {
-        // This class is internal, and you probably should use another impl
-        return new BASE64Decoder().decodeBuffer(property);
-    }
-    private static String encrypt(String property) throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException {
-        char[] PASSWORD = getPassword().toCharArray();
-        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithSHA1AndDESede");
-        SecretKey key = keyFactory.generateSecret(new PBEKeySpec(PASSWORD));
-        Cipher pbeCipher = Cipher.getInstance("PBEWithSHA1AndDESede");
-        pbeCipher.init(Cipher.ENCRYPT_MODE, key, new PBEParameterSpec(SALTSEQUENCE, 20));
-        return base64Encode(pbeCipher.doFinal(property.getBytes("UTF-8")));
+    public static boolean hashCheck(char[] password, byte[] salt, byte[] hashedPassword) throws GeneralSecurityException {
+        byte[] hashAttempt = generateHash(password, salt);
+        Arrays.fill(password, Character.MIN_VALUE);
+        if (hashAttempt.length != hashedPassword.length) {
+            return false;
+        }
+        for (int i = 0; i < hashAttempt.length; i++) {
+            if (hashAttempt[i] != hashedPassword[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    private static String decrypt(String property) throws GeneralSecurityException, IOException, FileNotFoundException {
-        char[] PASSWORD = getPassword().toCharArray();
-        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
-        SecretKey key = keyFactory.generateSecret(new PBEKeySpec(PASSWORD));
-        Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDES");
-        pbeCipher.init(Cipher.DECRYPT_MODE, key, new PBEParameterSpec(SALTSEQUENCE, 20));
-        return new String(pbeCipher.doFinal(base64Decode(property)), "UTF-8");
+    public static byte[] hash(char[] password, byte[] salt) {
+        PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATIONCOUNT, KEYLENGTH);
+        Arrays.fill(password, Character.MIN_VALUE);
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            return skf.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
+        } finally {
+            spec.clearPassword();
+        }
     }
 }

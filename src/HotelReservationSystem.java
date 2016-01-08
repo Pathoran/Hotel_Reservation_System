dc6692ebf009cb79c49f41435e86419aa3950595
Pathoran <TZQ3CKK@UPS.COM>
2016-01-08 16:47:46 -0500

    Experimenting GUI functionality.  Rethink who the user is for this program.  Customers or employees?  That will determine how the reservation system GUI is designed

--- src/HotelReservationSystem.java
+++ src/HotelReservationSystem.java
@@ -1,6 +1,9 @@
+import com.sun.xml.internal.txw2.TXW;
+
 import javax.swing.*;
 
 import java.awt.*; //edit this later to take out wildcard
+import java.awt.event.*;
 
 /**
  * Created by WilliamBeckler on 1/5/2016.
@@ -9,29 +12,45 @@ public class HotelReservationSystem extends JFrame {
 
     private JLabel label1;
     private JLabel label2;
-    private JButton button;
+    private JLabel label3;
+    private JButton button1;
+    private JButton button2;
     private JTextField txtField;
     private ImageIcon image1;
     private ImageIcon image2;
+    private int x = 0, y = 0;
+    private int rando, guess;
+    private JMenuBar menubar ;
+    private JMenu file;
+    private JMenuItem exit;
 
     public static void main(String[] args) {
         HotelReservationSystem hotelGUI = new HotelReservationSystem();
         hotelGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //hotelGUI.setSize(800, 600);
-        hotelGUI.pack(); //like setSize, but fits everything you add into the window
         hotelGUI.setVisible(true);
+        hotelGUI.setSize(800, 600);
         hotelGUI.setTitle("Hotel Reservation System");
     }
 
     public HotelReservationSystem() {
         setLayout(new FlowLayout());
 
-        image1 = new ImageIcon(getClass().getResource("pikachu.jpg"));
-        label1 = new JLabel(image1);
-        add(label1);
+        menubar = new JMenuBar();
+        setJMenuBar(menubar);
 
-        image2 = new ImageIcon(getClass().getResource("pie.jpg"));
-        label2 = new JLabel(image2);
-        add(label2);
+        file = new JMenu("File");
+        menubar.add(file);
+
+        exit = new JMenuItem("Exit");
+        file.add(exit);
+
+        exitProc exi = new exitProc();
+        exit.addActionListener(exi);
+    }
+
+    public class exitProc implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            System.exit(0);
+        }
     }
 }
